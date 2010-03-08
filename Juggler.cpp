e573575701e594a5db030beff3f2385ea0f36ea7
Jay Foad <jay.foad@gmail.com>
2010-03-08 10:03:13 +0000

    Draw lower arms.

--- Juggler.cpp
+++ Juggler.cpp
@@ -9,12 +9,42 @@ namespace
 	const double gravity = 9.80665;
 	const double dwellRatio = 0.3;
 	const double ballRadius = 0.05;
+	const double elbowX[NumberOfHands] = { -0.25, 0.25 };
+	const double throwX[NumberOfHands] = { -0.10, 0.10 };
+	const double catchX[NumberOfHands] = { -0.50, 0.50 };
 }
 
 Juggler::Juggler(const SiteswapPattern& p) :
 	pattern(p),
 	beatsPerSecond(4.0) // ??? 2.0 for <= 3 balls, 5.5 for >= 9 balls ?
 {
+	// Set up key points for hand movements.
+	for (size_t i = 0; i != pattern.getBeats(); ++i)
+	{
+		for (size_t h = 0; h != NumberOfHands; ++h)
+		{
+			const SiteswapHand::ThrowsType& throws =
+				pattern.getPattern()[i][h].getThrows();
+			for (size_t j = 0; j != throws.size(); ++j)
+			{
+				const SiteswapThrow& t = throws[j];
+				if (t.getHeight() > 2 || t.getCross() != 0)
+				{
+					// Throw
+					KeyPoint k;
+					k.b = i + 2 * dwellRatio;
+					k.x = throwX[h];
+					handKeyPoints[h].insert(k);
+
+					// Catch
+					size_t hh = (h + t.getCross()) % NumberOfHands;
+					k.b = (i + t.getHeight()) % pattern.getBeats();
+					k.x = catchX[hh];
+					handKeyPoints[hh].insert(k);
+				}
+			}
+		}
+	}
 }
 
 void Juggler::getBoundingBox(/* out */ double& left, double& right,
@@ -38,6 +68,37 @@ void Juggler::render(Canvas& c, double t) const
 	assert(t >= 0);
 	t = std::fmod(t, pattern.getBeats() / beatsPerSecond);
 
+	// Work out hand positions
+	double handX[NumberOfHands];
+	for (size_t h = 0; h != NumberOfHands; ++h)
+	{
+		if (handKeyPoints[h].empty())
+		{
+			handX[h] = elbowX[h];
+		}
+		else
+		{
+			KeyPoint needle;
+			needle.b = t * beatsPerSecond;
+			std::set<KeyPoint>::const_iterator it =
+				handKeyPoints[h].upper_bound(needle);
+			const KeyPoint& k2 = it == handKeyPoints[h].end()
+				? *handKeyPoints[h].begin() : *it;
+			const KeyPoint& k1 = it == handKeyPoints[h].begin()
+				? *handKeyPoints[h].rbegin() : *(--it);
+
+			double b1 = k1.b;
+			double b2 = k2.b;
+			if (b1 > needle.b)
+				b1 -= pattern.getBeats();
+			assert(b1 <= needle.b);
+			if (b2 <= needle.b)
+				b2 += pattern.getBeats();
+			assert(needle.b < b2);
+			handX[h] = k1.x + (needle.b - b1) / (b2 - b1) * (k2.x - k1.x);
+		}
+	}
+
 	// Head
 	c.circle(0.00, 0.70, 0.10);
 
@@ -57,13 +118,12 @@ void Juggler::render(Canvas& c, double t) const
 	c.lineTo(0.00, -0.10);
 	c.lineTo(+0.25, -0.80);
 
-	// Upper arms
-	c.moveTo(-0.25, 0.00);
+	// Arms
+	c.moveTo(handX[0], 0.00);
+	c.lineTo(-0.25, 0.00);
 	c.lineTo(0.00, 0.50);
 	c.lineTo(+0.25, 0.00);
-
-	// Lower arms
-	// ???
+	c.lineTo(handX[1], 0.00);
 
 	// Draw juggler.
 	c.stroke();
@@ -101,9 +161,6 @@ void Juggler::renderThrow(Canvas& c, size_t start, size_t hand,
 
 	for (; time < throwEndTime; time += period)
 	{
-		const double throwX[NumberOfHands] = { -0.10, 0.10 };
-		const double catchX[NumberOfHands] = { -0.50, 0.50 };
-
 		double x1 = throwX[hand];
 		double x2 = catchX[(hand + t.getCross()) % NumberOfHands];
 		double x = x1
--- Juggler.h
+++ Juggler.h
@@ -1,14 +1,27 @@
 #ifndef JUGGLER_H
 #define JUGGLER_H
 
+#include <set>
+
 #include "Siteswap.h"
 
 class Canvas;
 
 class Juggler
 {
+	struct KeyPoint
+	{
+		double b;
+		double x;
+		double vx;
+		double vy;
+
+		bool operator< (const KeyPoint& rhs) const { return b < rhs.b; }
+	};
+
 	SiteswapPattern pattern;
 	double beatsPerSecond;
+	std::set<KeyPoint> handKeyPoints[NumberOfHands];
 
 public:
 	// Constructor.
@@ -23,7 +36,7 @@ public:
 
 protected:
 	void renderThrow(Canvas& c, size_t start, size_t hand,
-		const SiteswapThrow& t, double b) const;
+		const SiteswapThrow& t, double time) const;
 };
 
 #endif // JUGGLER_H
